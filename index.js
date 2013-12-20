07609bf9deabb82f8f4354f9db9a91045f310644
feeloc <hjm1989@gmail.com>
2013-12-20 18:57:15 +0800

    实现了接收消息接口

--- index.js
+++ index.js
@@ -1,16 +1,311 @@
-var Weixin = function (options) {
+var sha1 = require('sha1');
+var BufferHelper = require('bufferhelper');
+var xml2js = require('xml2js');
+var events = require('events');
+var emitter = new events.EventEmitter();
 
+/**
+ * 微信类，实现微信的所有接口
+ * @param options
+ * @constructor
+ */
+var Weixin = function (options) {
+    this.url = options.url || '/';
+    this.token = options.token || '';
 };
 
-Weixin.prototype.init = function (options) {
+/**
+ * 初使化
+ * @param options
+ * {
+ *     token: ''    //自己在公共平台填写的token值
+ * }
+ * @returns {Weixin}
+ */
+Weixin.init = function (options) {
     return new Weixin(options);
 };
 
 /**
+ * 服务器请请该接口，发送三个参数，本地经过流程判断有效性
  * 验证权限
+ * @param req
+ * @returns {boolean}
  */
-Weixin.prototype.signature = function () {
-    console.log('测试方法');
+Weixin.prototype.signature = function (req) {
+    var signature = req.query.signature;    //服务器签名
+    var timestamp = req.query.timestamp;
+    var nonce = req.query.nonce;
+
+    var tmpArray = [this.token, timestamp, nonce];
+    tmpArray.sort();
+
+    this.sha1Str = sha1(tmpArray.join(''));
+
+    if (this.sha1Str == signature) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+/**
+ * 解析消息错误
+ * @returns {*}
+ */
+Weixin.prototype.handleMsgErr = function () {
+    emitter.on('MsgErr', this.err);
+    return this;
+};
+
+/**
+ * 监听解析消息错误
+ * @param callback
+ * @returns {*}
+ */
+Weixin.prototype.errMsg = function (callback) {
+    emitter.on('MsgErr', callback);
+    return this;
+};
+
+/**
+ * 处理文本消息
+ * @returns {*}
+ */
+Weixin.prototype.handleTextMsg = function () {
+    emitter.emit('TextMsg', this.msg);
+    return this;
+};
+
+/**
+ * 监听文本消息
+ * @param callback
+ * @returns {*}
+ */
+Weixin.prototype.textMsg = function (callback) {
+    emitter.on('TextMsg', callback);
+    return this;
+};
+
+/**
+ * 处理图片消息
+ * @returns {*}
+ */
+Weixin.prototype.handleImageMsg = function () {
+    emitter.emit('ImageMsg', this.msg);
+    return this;
+};
+
+/**
+ * 监听图片消息
+ * @param callback
+ * @returns {*}
+ */
+Weixin.prototype.imageMsg = function (callback) {
+    emitter.on('ImageMsg', callback);
+    return this;
+};
+
+/**
+ * 处理语音消息
+ * @returns {*}
+ */
+Weixin.prototype.handleVoiceMsg = function () {
+    emitter.emit('VoiceMsg', this.msg);
+    return this;
+};
+
+/**
+ * 监听语音消息
+ * @param callback
+ * @returns {*}
+ */
+Weixin.prototype.voiceMsg = function (callback) {
+    emitter.on('VoiceMsg', callback);
+    return this;
+};
+
+/**
+ * 处理视频消息
+ * @returns {*}
+ */
+Weixin.prototype.handleVideoMsg = function () {
+    emitter.emit('VideoMsg', this.msg);
+    return this;
+};
+
+/**
+ * 监听视频消息
+ * @param callback
+ * @returns {*}
+ */
+Weixin.prototype.videoMsg = function (callback) {
+    emitter.on('VideoMsg', callback);
+    return this;
+};
+
+/**
+ * 处理位置消息
+ * @returns {*}
+ */
+Weixin.prototype.handleLocationMsg = function () {
+    emitter.emit('LocationMsg', this.msg);
+    return this;
+};
+
+/**
+ * 监听位置消息
+ * @param callback
+ * @returns {*}
+ */
+Weixin.prototype.locationMsg = function (callback) {
+    emitter.on('LocationMsg', callback);
+    return this;
+};
+
+/**
+ * 处理位置消息
+ * @returns {*}
+ */
+Weixin.prototype.handleLinkMsg = function () {
+    emitter.emit('LinkMsg', this.msg);
+    return this;
+};
+
+/**
+ * 监听位置消息
+ * @param callback
+ * @returns {*}
+ */
+Weixin.prototype.linkMsg = function (callback) {
+    emitter.on('LinkMsg', callback);
+    return this;
+};
+
+/**
+ * 处理事件消息
+ * @returns {*}
+ */
+Weixin.prototype.handleEventMsg = function () {
+    var event = this.msg.Event;
+    switch (event) {
+        case 'subscribe':
+            this.handleSubEventMsg();   //订阅事件消息
+            break;
+        case 'unsubscribe':
+            this.handleUnsubEventMsg(); //取消订阅事件消息
+            break;
+    }
+
+    return this;
+};
+
+/**
+ * 处理订阅事件消息
+ * @returns {*}
+ */
+Weixin.prototype.handleSubEventMsg = function () {
+    emitter.emit('SubEventMsg', this.msg);
+    return this;
+};
+
+/**
+ * 监听订阅事件消息
+ * @param callback
+ * @returns {*}
+ */
+Weixin.prototype.subEventMsg = function (callback) {
+    emitter.on('SubEventMsg', callback);
+    return this;
+};
+
+/**
+ * 处理取消订阅事件消息
+ * @returns {*}
+ */
+Weixin.prototype.handleUnsubEventMsg = function () {
+    emitter.emit('UnSubEventMsg', this.msg);
+    return this;
+};
+
+/**
+ * 监听取消订阅事件
+ * @param callback
+ * @returns {*}
+ */
+Weixin.prototype.unsubEventMsg = function (callback) {
+    emitter.on('UnSubEventMsg', callback);
+    return this;
+};
+
+/**
+ * 接收消息解析
+ */
+Weixin.prototype.analysisMsg = function () {
+    if (this.err) {
+        this.handleMsgErr();
+    } else {
+        var MsgType = this.msg.MsgType;
+        switch (MsgType) {
+            case 'text':
+                this.handleTextMsg();   //文本消息
+                break;
+            case 'image':
+                this.handleImageMsg();  //图片消息
+                break;
+            case 'voice':
+                this.handleVoiceMsg();  //语音消息
+                break;
+            case 'video':
+                this.handleVideoMsg();  //视频消息
+                break;
+            case 'location':
+                this.handleLocationMsg();   //地理位置消息
+                break;
+            case 'link':
+                this.handleLinkMsg();   //链接消息
+                break;
+            case 'event':
+                this.handleEventMsg();
+                break;
+        }
+    }
+};
+
+/**
+ * 洗下数据，数组取第一个
+ * @param json
+ * @returns {*}
+ */
+Weixin.prototype.parseJson = function (json) {
+    for (var key in json) {
+        json[key] = json[key] instanceof Array ? json[key][0] : json[key];
+    }
+    return json;
+};
+
+/**
+ * 接收消息
+ * @param req
+ * @param res
+ */
+Weixin.prototype.getMsg = function (req) {
+    var bufferHelper = new BufferHelper();
+    var _this = this;
+    req.setEncoding('utf-8');
+    req.on('data', function (chunk) {
+        bufferHelper.concat(chunk);
+    });
+    req.on('end', function () {
+        xml2js.parseString(bufferHelper.toBuffer().toString(), function (err, json) {
+            if (err) {
+                _this.err = err;
+            } else {
+                _this.msg = _this.parseJson(json.xml);
+            }
+            _this.analysisMsg();
+        });
+    });
 };
 
 module.exports = Weixin;
